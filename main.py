5ccda05bbd961d644d24e82596d0c72887980415
t1101675 <gu-yx17@mails.tsinghua.edu.cn>
2019-05-14 22:02:48 +0800

    padding data

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from parse_args import args
 import model
 import os
 import json
+import torch
+import torch.optim as optim
+from torch.nn.utils.rnn import pad_sequence
+from tqdm import tqdm
 
 def process_data(dir):
     word_2_id = {}
@@ -11,7 +15,7 @@ def process_data(dir):
                 continue
             f = open(os.path.join(dir, filename), "r")
             raw = f.readlines()
-            id = 0
+            id = 1
             for item in raw:
                 tempL_passage = item.strip().split("\t")[2].split(" ")
                 for word in tempL_passage:
@@ -25,54 +29,66 @@ def load_word_2_id(dir):
     f = open(os.path.join(dir, "word_2_id.json"), "r")
     return json.loads(f.read())
 
-def load_data(filename, word_2_id):
+def format_batch_rnn(data, batch_size):
+    num = len(data) // batch_size
+    print(num)
+    passage_data = []
+    target_data = []
+    lengths = []
+    for k in range(0, num):
+        batch = data[k: k + batch_size]
+        batch = sorted(batch, key=lambda item: len(item[0]), reverse=True)
+        padded_passage = pad_sequence([torch.FloatTensor(item[0]) for item in batch], batch_first=True)
+        passage_data.append(padded_passage)
+        lengths.append(torch.FloatTensor([len(item[0]) for item in batch]))
+        target_data.append(torch.FloatTensor([item[1] for item in batch]))
+    return passage_data, target_data, lengths
+
+
+def load_data(filename, word_2_id, batch_size):
     # [{index:str, passage:[], total:int, class:[]}]
     f = open(filename, "r")
     raw = f.readlines()
     #print(raw)
     data = []
     for item in raw:
-        item_dict = {}
         tempL = item.strip().split("\t")
-        item_dict["index"] = tempL[0]
         tempL_passage = tempL[2].strip().split(" ")
-        item_dict["passage"] = [word_2_id[word] for word in tempL_passage]
-        tempL_class = tempL[1].strip().split(" ")
-        item_dict["total"] = int(tempL_class[0].split(":")[1])
-        item_dict["class"] = [int(x.split(":")[1]) for x in tempL_class[1:]]
-        data.append(item_dict)
+        tempL_target = tempL[1].strip().split(" ")
+        total = float(tempL_target[0].split(":")[1])
+        target_L = [float(x.split(":")[1]) / total for x in tempL_target[1:]]
+        passage_L = [word_2_id[word] for word in tempL_passage]
+        data.append((passage_L, target_L))
     return data
 
 def main():
-    if (args.test_loader):
-        word_2_id = load_word_2_id(args.data_dir)
-        data = load_data(os.path.join(args.data_dir, "train.in"), word_2_id)
-        print(data)
+    if (args.process_data):
+        process_data(args.data_dir)
         return
-
-
+    word_2_id = load_word_2_id(args.data_dir)
     if (args.model == "rnn"):
-        myModel = model.RNNModel(args)
+        myModel = model.RNNModel(args, len(word_2_id), 8)
     elif (args.model == "cnn"):
-        myModel = model.CNNModel(args)
+        myModel = model.CNNModel(args, len(word_2_id))
     else:
         print("invalid model type")
         exit(1)
 
     if (args.test_only):
-        test(myModel, args)
+        test(myModel, word_2_id, args)
     else:
-        train(myModel, args)
+        train(myModel, word_2_id, args)
     
 
-def train(model, args):
-    word_2_id = load_word_2_id(args.data_dir)
-    train_data = load_data(os.path.join(args.data_dir, "train.in"), word_2_id)
-    pass
+def train(model, word_2_id, args):
+    train_data = load_data(os.path.join(args.data_dir, "train.in"), word_2_id, args.batch_size)
+    print(len(train_data))
+    if (args.model == "rnn"):
+        passage_data, target_data, lengths = format_batch_rnn(train_data, args.batch_size)
+        print(lengths)
 
-def test(model, args):
-    word_2_id = load_word_2_id(args.data_dir)
-    test_data = load_data(os.path.join(args.data_dir, "test.in"), word_2_id)
+def test(model, word_2_id, args):
+    test_data = load_data(os.path.join(args.data_dir, "test.in"), word_2_id, args.batch_size)
     pass
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,18 +1,35 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class RNNModel(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, vocab_size, target_size):
         super(RNNModel, self).__init__()
-        self.class_dim = args.class_dim
         self.batch_size = args.batch_size
-        pass
+        self.vocab_size = vocab_size
+        self.target_size = target_size
+        self.hidden_dim = args.hidden_dim
+        self.dropout = args.dropout
+        self.embedding_size = args.embedding_size
+        self.num_layers = args.num_layers
+
+        self.lstm = nn.LSTM(input_size = self.embedding_size, hidden_size = self.hidden_dim, dropout = self.dropout, num_layers = self.num_layers)
+        self.hidden_2_target = nn.Linear(self.hidden_dim, self.target_size)
+        self.hidden = self.init_hidden()
+        self.word_embeddings = nn.Embedding(self.vocab_size, self.embedding_size)
+        
+
+    def init_hidden(self):
+        return torch.zeros(self.num_layers, self.batch_size, self.hidden_dim)
 
     def forward(self, batch):
-        
-        return 
+        embeds = self.word_embeddings(batch)
+        out, _ = self.lstm(embeds, self.hidden)
+        target_space = self.hidden_2_target(out)
+        score = F.log_softmax(target_space)
+        return score
 
 class CNNModel(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, vocab_size):
         super(CNNModel, self).__init__()
         pass
